62e11506d6346e49672295e29f5d0aece608c3fe
niki <niki@localhost-live.home>
2024-02-02 22:06:59 +0100

    Added another mesh & matrix generator, a tetrahedron and a shear matrix

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 
 #define DEFAULT_CHAR '#'
 #define PI 3.141592654
+#define TIME 30
 
 #define AXIS_X 0
 #define AXIS_Y 1
@@ -42,6 +43,83 @@ typedef struct {
   int edges_length;
 } Mesh;
 
+Mesh gen_cube_mesh() {
+  Mesh mesh;
+  mesh.verticies_length= 8;
+  mesh.edges_length = 12;
+  mesh.verticies = (Vector3*)calloc(8, sizeof(Vector3));
+  mesh.edges = (Vector2i*)calloc(12, sizeof(Vector2));
+
+  Mesh mesh_temp;
+
+  mesh_temp.verticies = (Vector3[]){
+    {-1.0, -1.0, -1.0}, // 0
+    { 1.0, -1.0, -1.0}, // 1
+    {-1.0,  1.0, -1.0}, // 2
+    { 1.0,  1.0, -1.0}, // 3
+    {-1.0, -1.0,  1.0}, // 4
+    { 1.0, -1.0,  1.0}, // 5
+    {-1.0,  1.0,  1.0}, // 6
+    { 1.0,  1.0,  1.0}, // 7
+  };
+
+  mesh_temp.edges = (Vector2i[]) {
+    {0, 1},
+    {0, 2},
+    {0, 4},
+    {1, 3},
+    {1, 5},
+    {2, 3},
+    {2, 6},
+    {3, 7},
+    {4, 5},
+    {4, 6},
+    {5, 7},
+    {6, 7},
+  };
+
+  for (int i = 0; i < mesh.verticies_length; i++) {
+    mesh.verticies[i] = mesh_temp.verticies[i];
+  }
+  for (int i = 0; i < mesh.edges_length; i++) {
+    mesh.edges[i] = mesh_temp.edges[i];
+  }
+  return mesh;
+}
+Mesh gen_tetrahedron_mesh() {
+  Mesh mesh;
+  mesh.verticies_length= 4;
+  mesh.edges_length = 6;
+  mesh.verticies = (Vector3*)calloc(4, sizeof(Vector3));
+  mesh.edges = (Vector2i*)calloc(6, sizeof(Vector2));
+
+  Mesh mesh_temp;
+
+  mesh_temp.verticies = (Vector3[]){
+    { 1.125, -0.865, -1},
+    { 1.125, -0.865,  1},
+    { 1.125,  0.865,  0},
+    {-1.125,      0,  0},
+  };
+
+  mesh_temp.edges = (Vector2i[]) {
+    {0, 1},
+    {0, 2},
+    {0, 3},
+    {1, 2},
+    {1, 3},
+    {2, 3},
+  };
+
+  for (int i = 0; i < mesh.verticies_length; i++) {
+    mesh.verticies[i] = mesh_temp.verticies[i];
+  }
+  for (int i = 0; i < mesh.edges_length; i++) {
+    mesh.edges[i] = mesh_temp.edges[i];
+  }
+  return mesh;
+}
+
 typedef struct {
   float matrix[3][3];
 } Matrix3;
@@ -63,6 +141,25 @@ Matrix3 gen_scale_matrix(float x, float y, float z) {
   m.matrix[0][0] = x;
   m.matrix[1][1] = y;
   m.matrix[2][2] = z;
+  return m;
+}
+
+Matrix3 gen_scale_matrix_uniform(float scale) {
+  return gen_scale_matrix(scale, scale, scale);
+}
+
+Matrix3 gen_shear_matrix(float a, float b) {
+  Matrix3 m;
+  float mat[3][3] = {
+    {1, a, b},
+    {0, 1, 0},
+    {0, 0, 1},
+  };
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      m.matrix[i][j] = mat[i][j];
+    }
+  }
   return m;
 }
 
@@ -238,51 +335,32 @@ int main() {
   curs_set(0);
   keypad(stdscr, true);
 
-  Mesh mesh;
-  mesh.edges_length = 12;
-  mesh.verticies_length= 8;
+  Mesh mesh1 = gen_cube_mesh();
 
-  mesh.verticies = (Vector3[]){
-    {-1.0, -1.0, -1.0}, // 0
-    { 1.0, -1.0, -1.0}, // 1
-    {-1.0,  1.0, -1.0}, // 2
-    { 1.0,  1.0, -1.0}, // 3
-    {-1.0, -1.0,  1.0}, // 4
-    { 1.0, -1.0,  1.0}, // 5
-    {-1.0,  1.0,  1.0}, // 6
-    { 1.0,  1.0,  1.0}, // 7
-  };
-
-  mesh.edges = (Vector2i[]) {
-    {0, 1},
-    {0, 2},
-    {0, 4},
-    {1, 3},
-    {1, 5},
-    {2, 3},
-    {2, 6},
-    {3, 7},
-    {4, 5},
-    {4, 6},
-    {5, 7},
-    {6, 7},
-  };
-
-  Matrix3 rotationX = gen_rotation_matrix(2.0, AXIS_X);
-  Matrix3 rotationY = gen_rotation_matrix(1.0, AXIS_Y);
-  Matrix3 rotationZ = gen_rotation_matrix(3.0, AXIS_Z);
+  float speed = 3.0;
+  Matrix3 rotationX = gen_rotation_matrix(speed, AXIS_X);
+  Matrix3 rotationY = gen_rotation_matrix(speed, AXIS_Y);
+  Matrix3 rotationZ = gen_rotation_matrix(speed, AXIS_Z);
 
 
-  mesh = apply_matrix_to_mesh(mesh, gen_scale_matrix(15, 15, 15));
-  while (true) {
+  mesh1 = apply_matrix_to_mesh(mesh1, gen_scale_matrix_uniform(15));
+
+  bool should_stop = false;
+  int counter = 0;
+  while (!should_stop) {
     clear();
-    mesh = apply_matrix_to_mesh(mesh, rotationX);
-    mesh = apply_matrix_to_mesh(mesh, rotationY);
-    // mesh = apply_matrix_to_mesh(mesh, rotationZ);
-    render_mesh(&mesh, 160.0);
+    counter++;
+    if (counter >= TIME * 10) should_stop = true;
+    mesh1 = apply_matrix_to_mesh(mesh1, rotationX);
+    mesh1 = apply_matrix_to_mesh(mesh1, rotationY);
+    mesh1 = apply_matrix_to_mesh(mesh1, rotationZ);
+    render_mesh(&mesh1, 160.0);
     refresh();
     msleep(100);
+    // getch();
   }
+
+  free(mesh1.verticies); free(mesh1.edges);
 
   endwin();
 }
